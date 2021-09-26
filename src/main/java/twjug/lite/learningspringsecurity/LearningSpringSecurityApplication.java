dcc1836af4016a3f60b65ed4c8dc12e14755d8d0
Ching Yi, Chan <chingyichan.tw@gmail.com>
2021-09-26 23:55:07 +0800

    Init
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

---
+++ src/main/java/twjug/lite/learningspringsecurity/LearningSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package twjug.lite.learningspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringSecurityApplication.class, args);
+	}
+
+}
