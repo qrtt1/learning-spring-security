2276494a06fdea68b4d74a49c73ad37e6772ab57
Ching Yi, Chan <chingyichan.tw@gmail.com>
2021-09-27 00:36:36 +0800

    Configure web security paths
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- src/main/java/twjug/lite/learningspringsecurity/MySecurityConfig.java
+++ src/main/java/twjug/lite/learningspringsecurity/MySecurityConfig.java
@@ -1,2 +1,17 @@
-package twjug.lite.learningspringsecurity;public class MySecurityConfig {
+package twjug.lite.learningspringsecurity;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/users**").authenticated()
+                .anyRequest().permitAll();
+    }
 }
