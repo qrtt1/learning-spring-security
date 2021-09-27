d00ea8e0741437870e208ce898b55e1364052f11
Ching Yi, Chan <chingyichan.tw@gmail.com>
2021-09-27 20:54:28 +0800

    Add ADMIN role
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- src/main/java/twjug/lite/learningspringsecurity/FriendlyFilter.java
+++ src/main/java/twjug/lite/learningspringsecurity/FriendlyFilter.java
@@ -1,5 +1,7 @@
 package twjug.lite.learningspringsecurity;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -8,11 +10,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class FriendlyFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        SecurityContextHolder.getContext().setAuthentication(new ApiToken());
+        SecurityContextHolder.getContext().setAuthentication(new ApiToken() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return Arrays.asList(
+                        new SimpleGrantedAuthority("ROLE_ADMIN"));
+            }
+        });
         filterChain.doFilter(request, response);
     }
 }
